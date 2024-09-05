5eb2cb756008dd56978a9a85364169eb4b2d0996
Owen Voke <development@voke.dev>
2024-09-05 14:02:10 +0100

    feat: change to deferrable provider

--- src/UkTaxAllowanceServiceProvider.php
+++ src/UkTaxAllowanceServiceProvider.php
@@ -2,16 +2,23 @@
 
 namespace Worksome\UkTaxAllowance;
 
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use Worksome\UkTaxAllowance\Calendars\YasumiUkCalendar;
 use Worksome\UkTaxAllowance\Contracts\UkCalendar;
 
-class UkTaxAllowanceServiceProvider extends ServiceProvider
+class UkTaxAllowanceServiceProvider extends ServiceProvider implements DeferrableProvider
 {
-    public function register()
+    public function register(): void
     {
         $this->app->bind(UkCalendar::class, function () {
             return new YasumiUkCalendar();
         });
+    }
+
+    /** @return array<class-string> */
+    public function provides(): array
+    {
+        return [UkCalendar::class];
     }
 }
